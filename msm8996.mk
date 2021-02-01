c1f40c16dd54ccd884125c371bba69190a88395d
aswin7469 <aswinas7469@gmail.com>
2021-02-01 05:30:47 +0000

    msm8996-common: build libcnefeatureconfig
    Signed-off-by: aswin7469 <aswinas7469@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -361,6 +361,7 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
     android.hardware.radio.config@1.0 \
     librmnetctl \
+    libcnefeatureconfig \
     libxml2
 
 # Seccomp policy
