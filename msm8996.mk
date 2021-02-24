a64f1363b4a3a7e905012a52ee9c3ffada30f44c
aswin7469 <aswinas7469@gmail.com>
2021-02-24 06:55:30 +0000

    msm8996-common: update Graphics from LA.UM.9.6.2.r1-03600-89xx.0

--- msm8996.mk
+++ msm8996.mk
@@ -168,11 +168,11 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
+		    android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.1-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     vendor.display.color@1.0-impl \
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -86,6 +86,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=skiavk \
+    ro.hardware.vulkan=adreno \
+    ro.hardware.egl=adreno \
     debug.egl.hw=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
