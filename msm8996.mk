03a90ac82cb828829190551558c082a7a35b6a39
triHims <himanshu.16bcs1156@abes.ac.in>
2021-02-26 11:11:35 +0000

    msm8996-common: fixed \ in the soundtrigger entry.

--- msm8996.mk
+++ msm8996.mk
@@ -107,7 +107,7 @@ PRODUCT_PACKAGES += \
 android.hardware.audio@6.0-impl:32 \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32 \    
+    android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
